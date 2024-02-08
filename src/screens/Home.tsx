a1e2a2132df0ce3662956bc50a2211f4a48d59c2
Kayque Lopes <104032451+KayqueSekishiki@users.noreply.github.com>
2024-02-08 19:41:34 -0300

    Estruturando arquivos!

---
+++ src/screens/Home.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text> Home works!!</Text>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
