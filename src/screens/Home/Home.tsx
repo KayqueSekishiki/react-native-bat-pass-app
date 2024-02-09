9912d3f6963f2df216b6faf8f140ab2ade98b026
Kayque Lopes <104032451+KayqueSekishiki@users.noreply.github.com>
2024-02-08 21:33:39 -0300

    Snippets

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 import styles from "../Home//Style";
+import { Menu } from "../../components/Menu/Menu";
 
 export default function Home() {
   return (
     <View style={styles.container}>
+      <Menu />
       <Text> Home works!!</Text>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
