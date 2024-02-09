2ae3c402e523afacea32b4a58f214ae159d56644
Kayque Lopes <104032451+KayqueSekishiki@users.noreply.github.com>
2024-02-08 21:44:01 -0300

    Adicionado Logo

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 import styles from "../Home//Style";
-import { Menu } from "../../components/Menu/Menu";
+import { BatLogo } from "../../components/BatLogo/BatLogo";
 
 export default function Home() {
   return (
     <View style={styles.container}>
-      <Menu />
+      <View>
+        <BatLogo />
+      </View>
       <Text> Home works!!</Text>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
