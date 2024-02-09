fbf53001ee4960770becdb25b7127933d176357e
Kayque Lopes <104032451+KayqueSekishiki@users.noreply.github.com>
2024-02-08 22:00:52 -0300

    Adicionando caixa de texto

---
+++ src/components/BatTextInput/BatTextInput.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { TextInput } from "react-native";
+
+import { styles } from "./BatTextInputStyles";
+
+export function BatTextInput() {
+  return <TextInput style={styles.inputer} placeholder="pass" />;
+}
--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -3,12 +3,17 @@ import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 import styles from "../Home//Style";
 import { BatLogo } from "../../components/BatLogo/BatLogo";
+import { BatTextInput } from "../../components/BatTextInput/BatTextInput";
 
 export default function Home() {
   return (
     <View style={styles.appContainer}>
       <View style={styles.logoContainer}>
         <BatLogo />
+      </View>
+
+      <View>
+        <BatTextInput />
       </View>
       <Text> Home works!!</Text>
       <Text>Open up App.tsx to start working on your app!</Text>
