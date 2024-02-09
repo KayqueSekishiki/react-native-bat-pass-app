ba14f74fb3a6a45eaa14df4c3ba3b2b36771930b
Kayque Lopes <104032451+KayqueSekishiki@users.noreply.github.com>
2024-02-08 21:26:50 -0300

    Aplicando organização!

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
+import styles from "../Home//Style";
 
 export default function Home() {
   return (
@@ -11,12 +12,3 @@ export default function Home() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
